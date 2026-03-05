3bcd5926104d051cbbd3576e5b46be443092f462
Marc Alcaraz <ekameleon@gmail.com>
2026-03-05 09:38:37 +0100

    Update generate-exports.js

--- scripts/generate-exports.js
+++ scripts/generate-exports.js
@@ -4,7 +4,6 @@ import { fileURLToPath } from 'node:url';
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
-// Liste mise à jour selon ton ls -d src/*/
 const SRC_DIRS = [
     'components',
     'contexts',
@@ -12,34 +11,29 @@ const SRC_DIRS = [
     'helpers',
     'hooks',
     'motions',
-    'themes',
-    '@assets',
-    '@configs',
-    '@locale'
+    'themes'
 ];
 
 const exports = {
-    "./package.json": "./package.json"
+    "./package.json": "./package.json",
+    "./README.md": "./README.md",
+    "./LICENSE": "./LICENSE",
+    "./version": "./src/version.js",
+    "./src/*": "./src/*"
 };
 
-for ( const dir of SRC_DIRS )
-{
+for (const dir of SRC_DIRS) {
     const dirPath = join(__dirname, '../src', dir);
 
-    if ( existsSync( dirPath ) )
+    if (existsSync(dirPath))
     {
         exports[`./${dir}/*.css`] = `./src/${dir}/*.css`;
         exports[`./${dir}/*.js`] = `./src/${dir}/*.js`;
         exports[`./${dir}/*.jsx`] = `./src/${dir}/*.jsx`;
         exports[`./${dir}/*.mjs`] = `./src/${dir}/*.mjs`;
 
-        // Le wildcard général pour les imports sans extension
         exports[`./${dir}/*`] = `./src/${dir}/*`;
     }
-}
-
-if (existsSync(join(__dirname, '../src/index.js'))) {
-    exports["."] = "./src/index.js";
 }
 
 const pkgPath = join(__dirname, '../package.json');
@@ -48,4 +42,5 @@ const pkg = JSON.parse(readFileSync(pkgPath, 'utf8'));
 pkg.exports = exports;
 
 writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n');
-console.log(`✓ Generated ${Object.keys(exports).length} export mapping categories (pointing to /src).`);
+
+console.log(`✓ Exports générés (incluant README et LICENSE).`);
